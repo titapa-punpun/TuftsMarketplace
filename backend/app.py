3d9091a97568d3d9d77225b56d15ab81bd85112b
titapapunne <titapa.chaiyakiturajai@tufts.edu>
2020-03-17 16:31:32 -0400

    first commit

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import psycopg2
 
 app = Flask(__name__)
 api = Api(app)
 
+connection = psycopg2.connect(user="titapapunne",
+                              password="",
+                              host="127.0.0.1",
+                              port="5432",
+                              database="titapapunne")
+
+cursor = connection.cursor()
+# Print PostgreSQL Connection properties
+# print ( connection.get_dsn_parameters(),"\n")
+
 class HelloWorld(Resource):
     def get(self):
+        username = 'titapapunne'
+        cursor.execute("INSERT INTO table_1 (name) VALUES ('" + username + "');")
+        connection.commit()
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/hi')
+api.add_resource(HelloWorld, '/submitUser')
 
 if __name__ == '__main__':
     app.run(debug=True)
