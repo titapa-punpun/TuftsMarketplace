7f8d23591565a449b79a3ae8f1a8c53dbde6fecf
titapapunne <titapa.chaiyakiturajai@tufts.edu>
2020-04-11 11:34:34 -0400

    finished Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,25 +5,36 @@ class Home extends React.Component {
     /* creating constructor */
     constructor(props) {
         super(props);
+        this.state = {items: []}
     }
 
-    fetch('http://127.0.0.1:5000/getAllItems',
-    {method: 'GET'})
-        .then(response => response.json)
-        .then(
-            console.log('no data', x)
-            return('no data')
-        )
+    componentDidMount() {
+        fetch('http://127.0.0.1:5000/getAllItems',
+            {method: 'GET'})
+            // => separates parameter from body of lamda function. LHS is param, RHS is the body of func.
+            .then(response => response.json())
+            .then(response => {this.setState({items: response.allItems})})
+    }
 
-        })
-
+    // render() {
+    //     const l = [1, 2, 3, 4]
+    //         return (
+    //             <div>
+    //                 {l.map(x => (<div key={x}>{x}<br/></div>))}
+    //             </div>
+    //         );
+    // }
 
     render() {
-            return (
-                <div>
-                    Hello
-                </div>
-            );
+        const allItems = this.state.items;
+        return (
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                {allItems.map(item => (<div key={item.itemID}> Item: {item.itemName} <br/>
+                                       Description: {item.itemDescription} <br/>
+                                       Price: ${item.itemPrice} <br/>
+                                       Quantity: {item.itemQuantity} <br/> </div>))}
+            </div>
+        )
     }
 }
 
